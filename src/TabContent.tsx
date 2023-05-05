986a2e3cf0011bd3f4530857d563c0d57358a16f
Jaro987 <jaroslav.mucaji@gmail.com>
2023-05-05 23:09:14 +0200

    add api call and print stringified result

--- src/TabContent.tsx
+++ src/TabContent.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
+
 type TabContentType = {
     topic: string;
 }
 
 const TabContent = ({ topic }: TabContentType) => {
+    const url = `https://api.github.com/search/repositories?q=${topic}&sort=stars&order=desc`;
+    const [result, setResult] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+          const response = (await fetch(url));
+          const data = await response.json();
+          setResult(data);
+        }
+        fetchData();
+      }, [url]);
     return (
         <div>
-            {topic}
+            {JSON.stringify(result)}
         </div>
     );
 };
